63390a0dd278464afb68ed77429727ec33e006b4
andrea lee <lee.ays.lee@gmail.com>
2017-11-05 22:03:43 -0500

    server running

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// const flashcardRoutes = require('./routes/jobapps-routes');
+//   app.use('/jobapps/', jobappsRoutes);
+
+app.use('*', (req, res) => {
+  res.status(404).send('404 NOT FOUND');
+});
