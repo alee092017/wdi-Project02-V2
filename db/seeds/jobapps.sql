fb5c16883de25b77e9d403897e7f7d28ae5dcf92
andrea lee <lee.ays.lee@gmail.com>
2017-11-05 22:50:09 -0500

    fixed db name in config.js, db and tables look ok

---
+++ db/seeds/jobapps.sql
@@ -0,0 +1,34 @@
+INSERT INTO jobapps (status, company, jobtitle, linktoapp, datesubmitted, followup, intcontactname, intcontactemail, notes) VALUES
+  (
+    'In Person',
+    'Hanna-Barbera',
+    'Animation Artist',
+    'www.h-bCartoons.com/fakejob.app',
+    '2017-10-24',
+    'No',
+    'George Sidney',
+    'GSidney@noemail.com',
+    'no notes at this time.'
+  ), /*remember semicolon at last one*/
+  (
+    'Phone Screen',
+    'Cartoon Network',
+    'Animation coordinator',
+    'https://www.fakejobsrus.com/job/atlanta/creative-production-coordinator-cartoon-network/1174/6012773',
+    '2017-10-24',
+    'Yes',
+    'Ted Turner',
+    'TheTedTurner@tnt.com',
+    'coordinator position.  :(, awkward phone screen.  atlanta'
+  ),
+  (
+    'In Review',
+    'Toei Animation',
+    'Character Development Artist',
+    'https://www.morefakejobs.com/job/losangeles/characterDev/1174/6012773',
+    '2017-10-24',
+    'Yes',
+    'Kenzo Masaoka',
+    'KenzoM@voltron.com',
+    'Lots of notes writing them here.  many words'
+  );
